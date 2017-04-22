cd2c28eca25df0c1701600551e889f4054d610e9
aditya <aditya@adityabane>
2017-04-22 14:44:12 +0530

    1st Commit

---
+++ maven_test/src/main/java/com/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.controller;
+ 
+
+ 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+ 
+ 
+@Controller
+@RequestMapping("/users")
+public class UserController {
+ 
+    @RequestMapping(value = "/page", method = RequestMethod.GET)
+    public ModelAndView getPage() {
+        ModelAndView view = new ModelAndView("hello");
+        return view;
+    }
+ 
+}
